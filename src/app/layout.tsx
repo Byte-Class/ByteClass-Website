960c5e94e659ff878e7fed210affeb7672526be9
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-09-09 21:44:21 +1200

    feat: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import "./globals.css";
+
+import { ThemeProvider } from "@/components/theme-provider";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <header className="h-lvh w-full">
+      <div className="mx-auto flex h-full w-4/5 items-center justify-center">
+        <div className="w-2/5">
+          <h1 className="text-5xl font-bold">ByteClass</h1>
+          <p>
+            ByteClass a drag and drop replacement for all google classroom task
+            and student management. ByteClass offers a variety of student
+            management tools and advanced task and assignment management
+            features.
+          </p>
+        </div>
+        <div className="w-3/5">
+          <Image
+            src={"/splash.svg"}
+            alt={"Woman on laptop and she looks worried"}
+            width={1500}
+            height={1500}
+          />
+        </div>
+      </div>
+    </header>
+  );
+}
