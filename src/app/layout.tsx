9e41994719aa6e1db79b5d1736122232afbafb01
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-09-09 21:51:05 +1200

    feat: changed default theme from system to dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
       <body className={`antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
