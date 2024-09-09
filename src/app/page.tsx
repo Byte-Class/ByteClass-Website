6e597aaf47615f9219e935e808b44543fa1e992c
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-09-09 22:53:34 +1200

    feat: seperator and main part of home page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 
 import { Navbar } from "@/components/navbar";
+import { Testimonial } from "@/components/testimonial";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
   return (
@@ -38,6 +40,80 @@ export default function Home() {
           />
         </div>
       </header>
+
+      <main className="flex w-full flex-col gap-20 pb-8 pt-8">
+        <div className="mx-auto w-4/5">
+          <h2 className="text-center text-5xl font-bold">Testimonials</h2>
+
+          <div className="mt-20 flex w-full items-center justify-between gap-16">
+            <Testimonial
+              name="Siddhesh Zantye"
+              quote="This app changed my life."
+              image="siddhesh.jpg"
+            />
+            <Testimonial
+              name="Jauhar Goga"
+              quote="This app is the reason I did not kill myself. Without this app, I would be homeless, working for a homeless troll doll. This app saved me. Please use it :)"
+              image="jauhar.jpg"
+            />
+            <Testimonial
+              name="Siddharth Sahadew"
+              quote="This app is so good please use so I can get rich 🤑💸💰🐒"
+              image="siddharth.jpg"
+            />
+          </div>
+        </div>
+
+        <Separator className="mx-auto w-4/5" />
+
+        <div className="mx-auto flex w-4/5 items-center">
+          <h2 className="flex-1 text-left text-5xl font-bold">Who Are We?</h2>
+          <p className="flex-[3]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+            consectetur varius nunc nec ornare. Aliquam at diam justo. Aliquam
+            leo ante, elementum id auctor at, porttitor a ante. Nullam ultrices
+            congue ligula. Maecenas tincidunt nec risus eget euismod. Morbi ac
+            ligula arcu. Phasellus euismod elit vel libero tincidunt
+            sollicitudin. Fusce egestas dolor ut eros condimentum, id sagittis
+            magna finibus. Donec luctus fringilla mauris quis dignissim. Ut
+            iaculis augue diam, eget sollicitudin libero ullamcorper eget. Ut
+            libero augue, hendrerit eu sem a, venenatis ornare justo. Etiam
+            euismod dignissim nunc nec blandit. Praesent imperdiet non nisi
+            interdum porttitor. Aliquam varius ante eget.
+          </p>
+        </div>
+
+        <Separator className="mx-auto w-4/5" />
+
+        <div className="mx-auto w-4/5 text-center">
+          <h2 className="text-5xl font-bold">Our Mission</h2>
+          <p className="mt-8">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a elit
+            quis nulla venenatis mattis sed sit amet ex. In quis libero
+            tristique, rhoncus est a, auctor tortor. Suspendisse egestas enim
+            nec gravida ultrices. Phasellus ornare sem vitae enim accumsan, eu
+            varius turpis varius. Nam risus neque, ornare vitae mi sit amet,
+            fermentum vestibulum nisl. Curabitur dignissim ac sem ac eleifend.
+            Nulla pharetra, enim at ullamcorper egestas, nisi purus mollis
+            metus, a laoreet massa risus eu massa. Nam velit nibh, aliquet non
+            sollicitudin et, vehicula eu magna. Sed non pharetra augue. Integer
+            malesuada placerat metus ac gravida. Proin laoreet libero vitae
+            pellentesque viverra. Nulla facilisi. Aliquam luctus tincidunt
+            turpis, non accumsan nulla euismod a.
+          </p>
+          <p className="mt-4">
+            Curabitur at tristique ex, in efficitur purus. Aliquam quis
+            fringilla enim. In at arcu at quam rutrum aliquam quis sit amet
+            massa. Praesent vestibulum nisi quam, eu fringilla libero pulvinar
+            sagittis. Nulla facilisi. Nullam fermentum nulla nec augue viverra,
+            et faucibus metus euismod. Sed pulvinar egestas risus, consectetur
+            interdum purus imperdiet id. Nam volutpat, ligula et lacinia
+            scelerisque, lacus magna mattis metus, nec gravida lorem mauris vel
+            tortor. Vestibulum rhoncus massa orci, non vehicula quam eleifend
+            non. Praesent sed nulla dolor. Aenean sit amet leo vel.
+          </p>
+        </div>
+      </main>
     </>
   );
 }
---
+++ src/components/testimonial.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+
+import { Quote } from "lucide-react";
+
+export const Testimonial = ({
+  name,
+  quote,
+  image,
+}: {
+  name: string;
+  quote: string;
+  image: string;
+}) => {
+  return (
+    <div className="relative flex min-h-64 flex-1 flex-col justify-between rounded-xl bg-[#1a1a1a] p-4">
+      <div className="absolute -top-6 left-6 flex h-12 w-12 items-center justify-center rounded-full bg-white text-3xl">
+        <Quote className="rotate-180" color="black" strokeWidth={3} />
+      </div>
+      <div className="absolute -bottom-6 right-6 flex h-12 w-12 items-center justify-center rounded-full bg-white text-3xl">
+        <Quote color="black" strokeWidth={3} />
+      </div>
+
+      <Image
+        src={`/testimonials/${image}`}
+        className="absolute -top-12 left-1/2 -translate-x-1/2 rounded-full"
+        alt="Nerds"
+        height={96}
+        width={96}
+      />
+
+      <p className="mt-[15%] text-center">{quote}</p>
+      <h3> - {name}</h3>
+    </div>
+  );
+};
