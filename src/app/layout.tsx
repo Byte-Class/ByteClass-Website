3f810fb56a8852343282edd11940fa9d376014cc
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-09-09 21:57:38 +1200

    feat: navbar added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>
+      <body className="antialiased">
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
---
+++ src/components/navbar.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+
+export const Navbar = () => {
+  return (
+    <nav className="flex h-28 w-full items-center justify-between px-12 py-4">
+      <Image
+        src={"/byte.png"}
+        alt={"ByteClass logo"}
+        width={100}
+        height={100}
+      />
+
+      <Button className="px-12 py-8">Sign In</Button>
+    </nav>
+  );
+};
