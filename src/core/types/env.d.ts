1852ff6992dc112211f147c0c1946f8f06745384
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-09-17 07:45:06 +1200

    feat: environment variables

---
+++ src/core/types/env.d.ts
@@ -0,0 +1,14 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      GOOGLE_CLIENT_ID: string;
+      GOOGLE_CLIENT_SECRET: string;
+      GOOGLE_REDIRECT_URL: string;
+
+      ACCESS_TOKEN_SECRET: string;
+
+      DATABASE_URL: string;
+    }
+  }
+}
+export {};
