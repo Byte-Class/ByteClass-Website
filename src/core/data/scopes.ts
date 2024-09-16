ccc7078229451bbec63a850afe1dbed39f66d6d3
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-09-17 07:51:08 +1200

    feat: google scopes

---
+++ src/core/data/scopes.ts
@@ -0,0 +1,14 @@
+export const SCOPES = [
+  // General Email and Profile Scopes
+  "profile",
+  "email",
+  "openid",
+  // Courses Scope
+  "https://www.googleapis.com/auth/classroom.courses",
+  "https://www.googleapis.com/auth/classroom.courses.readonly",
+  // Coursework scopes
+  "https://www.googleapis.com/auth/classroom.coursework.students.readonly",
+  "https://www.googleapis.com/auth/classroom.coursework.me.readonly",
+  "https://www.googleapis.com/auth/classroom.coursework.students",
+  "https://www.googleapis.com/auth/classroom.coursework.me",
+];
