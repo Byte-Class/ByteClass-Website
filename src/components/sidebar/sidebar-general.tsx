fa0fdc5998aaf71afec9485ea1f5db48c1638227
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-09-20 22:11:59 +1200

    bug: fix bug with react keys

--- src/components/sidebar/sidebar-general.tsx
+++ src/components/sidebar/sidebar-general.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Link from "next/link";
 
 import { Separator } from "@/components/ui/separator";
@@ -22,7 +23,7 @@ export const SideBarGeneral = () => {
     <div className="flex w-full flex-col justify-center">
       {links.map((link) => {
         return (
-          <>
+          <React.Fragment key={link.title}>
             <Link
               href={`/dashboard/${link.href}`}
               className="text-lg font-bold"
@@ -30,7 +31,7 @@ export const SideBarGeneral = () => {
               {link.title}
             </Link>
             <Separator className="my-2 ml-2 w-[calc(100%-0.5rem)]" />
-          </>
+          </React.Fragment>
         );
       })}
     </div>
